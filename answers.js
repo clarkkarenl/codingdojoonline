e556bd4f0ae37b2e5fb4b94364ce4f08435a245d
karen <clarkkarenl@users.noreply.github.com>
2018-04-28 13:25:45 -0700

    Initial commit of answers.js

---
+++ answers.js
@@ -0,0 +1,104 @@
+// Coding Dojo Online Cohort April 30, 2018
+// Karen Clark
+//
+// Algorithm Book Chapter 1 Assignments
+
+///////////
+// Page 16 
+///////////
+
+// Setting and Swapping
+
+// Print and Count
+
+// Print -52 to 1066
+
+// Multiples of Six
+
+// Don't Worry, Be Happy
+
+// Counting the Dojo Way
+
+// Multiples of Three - But Not All
+
+// What Do You Know?
+
+// Printing Integers While
+
+// Whoa, That Sucker's Huge
+
+// You Say It's Your Birthday
+
+// Countdown by Fours
+
+// Leap Year
+
+// Flexible Countdown
+
+// The Final Countdown
+
+///////////////
+// END PAGE 16
+///////////////
+
+////////////
+// Page 20 
+////////////
+
+// Countdown
+
+// Print and Return
+
+// First Plus Length
+
+// Values Greater Than Second
+
+// Values Greater Than Second, Generalized
+
+// This Length, That Value
+
+// Fit the First Value
+
+// Fahrenheit to Celsius
+
+// Celsius to Fahrenheit
+
+///////////////
+// END PAGE 20
+///////////////
+
+///////////
+// Page 22
+///////////
+
+// Biggie Size
+
+// Previous Lengths
+
+// Print Low, Return High
+
+// Add Seven to Most
+
+// Print One, Return Another
+
+// Reverse Array
+
+// Double Vision
+
+// Outlook: Negative
+
+// Count Positives
+
+// Always Hungry
+
+// Evens and Odds
+
+// Swap Toward the Center
+
+// Increment the Seconds
+
+// Scale the Array
+
+///////////////
+// END PAGE 22
+///////////////
